636383fb5e08cae64590c1b07590b8a4e02482c7
discoform <mh97128@gmail.com>
2021-07-26 11:24:41 -0700

    add navigation menu

---
+++ js/expand-menu.js
@@ -0,0 +1,4 @@
+// <!-- Main Navigation Toggle -->
+function mainNavigation() { var x = document.getElementById("main-menu"); if (x.className === "topnav") { x.className += " responsive"; } else { x.className = "topnav"; } }
+// <!-- Mobile Footer Toggle -->
+function mobileFooter() { var x = document.getElementById("main-footer"); if (x.className === "site-footer") { x.className += " responsive"; } else { x.className = "site-footer"; } }
